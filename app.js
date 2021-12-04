17e64a9c66a5145dd5c73e6131a1b610e42b5ed7
Mays Majeed <mays.majeed2071@gmail.com>
2021-12-04 21:10:11 +0300

    adding files

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-var pattern = /^[a-zA-Z]{2}$/;
+// var pattern = /^[a-zA-Z]{2}$/;
 
-var submitButton = document.querySelector(".submit");
-submitButton.addEventListener("click", ValidateEmail);
+// var submitButton = document.querySelector(".submit");
+// submitButton.addEventListener("click", ValidateEmail);
 
-function ValidateEmail() {
-  var mytext = document.getElementById("emailAdress");
-  var myInput = document.getElementById("emailAdress").value;
-  var warrMessage = document.querySelector(".warr-message");
-  if (pattern.test(myInput)) {
-    mytext.style.borderColor = "green";
+// function ValidateEmail() {
+//   var mytext = document.getElementById("emailAdress");
+//   var myInput = document.getElementById("emailAdress").value;
+//   var warrMessage = document.querySelector(".warr-message");
+//   if (pattern.test(myInput)) {
+//     warrMessage.classList.add = "visiblex";
+//   }
+//   warrMessage.classList.remove = "visiblex";
+// }
+
+const submitBtn = document.querySelector(".submit");
+const emailInput = document.querySelector("#emailAdress");
+const errorImg = document.querySelector(".errImage");
+const errorMessag = document.querySelector(".warr-message");
+const REG_EX =
+  /^([a-zA-Z\d\.-_]+)@([a-zA-Z\d\.-]{2,8})\.([a-zA-Z]{2,8})(\.[a-zA-Z]{2,8})?$/;
+submitBtn.addEventListener("click", function (e) {
+  if (REG_EX.test(String(emailInput.value).toLowerCase())) {
+    errorImg.classList.remove("visible");
+    errorMessag.classList.remove("visible");
+  } else {
+    errorImg.classList.add("visible");
+    errorMessag.classList.add("visible");
   }
-  mytext.style.borderColor = "red";
-}
+});
